3a7b2b6f367d72929d96af945229eff7d7c17495
Chaoli <cl3080@nyu.edu>
2019-02-09 20:16:33 -0500

    add new file

---
+++ RemoveNthNodeFromEndofList.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeNthFromEnd(self, head: 'ListNode', n: 'int') -> 'ListNode':
+        dummy = ListNode(0)
+        dummy.next = head
+        p1 = p2 = dummy
+        for i in range(n):
+            p1 = p1.next
+        while p1.next:
+            p1 = p1.next
+            p2 = p2.next
+        p2.next = p2.next.next
+        return dummy.next
