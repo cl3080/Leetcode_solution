178af71d6b7da297cbbe8616c14c0265eb1e820d
Chaoli <cl3080@nyu.edu>
2019-02-15 18:53:20 -0500

    add file

---
+++ ValidPerfectSquare.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPerfectSquare(self, num: 'int') -> 'bool':
+        left, right = 0, num
+        while left <= right:
+            mid = (left+right)//2
+            if mid*mid >= num:
+                right = mid -1
+            else:
+                left = mid + 1
+        return left*left == num
