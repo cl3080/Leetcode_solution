5a2a649937535b02502e2aecae50f55cb829cb79
Chaoli <cl3080@nyu.edu>
2019-02-15 18:22:41 -0500

    add file

---
+++ OpenTheLock.py
@@ -0,0 +1,26 @@
+class Solution:
+    def openLock(self, deadends: 'List[str]', target: 'str') -> 'int':
+        from collections import deque
+        deadset = set(deadends)
+        if (target in deadset) or ("0000" in deadset): return -1
+        que = collections.deque()
+        que.append("0000")
+        visited = set(["0000"])
+        step = 0
+        while que:
+            step += 1
+            size = len(que)
+            for i in range(size):
+                point = que.popleft()
+                for j in range(4):
+                    for k in range(-1,2,2):
+                        newPoint = [i for i in point]
+                        newPoint[j] = chr((ord(newPoint[j]) - ord('0') + k + 10) % 10 + ord('0'))
+                        newPoint = "".join(newPoint)
+                        if newPoint == target:
+                            return step
+                        if (newPoint in deadset) or (newPoint in visited):
+                            continue
+                        que.append(newPoint)
+                        visited.add(newPoint)
+        return -1
